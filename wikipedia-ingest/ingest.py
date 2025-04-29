31545fdd702c6e4c5d3261124c6945b7189c401a
ayush chaurasia <ayush.chaurarsia@gmail.com>
2025-04-29 15:56:10 +0530

    many perf fixes and FTS create table

--- wikipedia-ingest/ingest.py
+++ wikipedia-ingest/ingest.py
@@ -28,13 +28,10 @@ app = modal.App("wikipedia-processor",
     #region="us-east" # Using east-east further speeds up the ingestion rate but provisioning takes more time
 )
 class ChunkProcessor:
-    table_name: str = modal.parameter()
-
-    @modal.enter()
-    def deserialize_parameters(self):
+    def __init__(self, table_name) -> None:
         disable_caching()
         self.ds = load_from_disk(DATASET_PATH)["train"]
-        self.embedder = WikipediaProcessor(table_name=self.table_name)
+        self.embedder = WikipediaProcessor(table_name=table_name)
 
     @modal.method()
     def process_batch(self, batch_indices: list):
@@ -72,15 +69,13 @@ class ChunkProcessor:
     #region="us-east", # Using east-east further speeds up the ingestion rate but provisioning takes more time
 )
 class WikipediaProcessor:
-    table_name: str = modal.parameter()
 
-    @modal.enter()
-    def deserialize_parameters(self):
+    def __init__(self, table_name):
         from sentence_transformers import SentenceTransformer
         
         self.model = SentenceTransformer(MODEL_NAME, device='cuda')
         self.model.eval()
-        self.table = get_or_create_table(self.table_name)
+        self.table = get_or_create_table(table_name)
 
     @modal.method()
     def process_chunks(self, chunks: List[Dict]) -> int:
@@ -129,7 +124,8 @@ def start(down_scale: float = 1.0, table_name: str = DEFAULT_TABLE_NAME):
 
     table = get_or_create_table(table_name)
     table.create_index(metric="dot", vector_column_name="emb", index_type="IVF_PQ")
-    #table.create_fts_index(["content", "title"])
+    table.create_fts_index("title", remove_stop_words=True)
+    table.create_fts_index("content", remove_stop_words=True)
 
 @app.local_entrypoint()
 def main(down_scale: float = 1.0, table_name: str = DEFAULT_TABLE_NAME):
